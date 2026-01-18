5c69884771c5826e9dbd8c32e5b2860d4b52b6f7
guillaume prof <guillaume.saintetienne@iter.org>
2026-01-18 17:28:52 +0100

    remove all source and test files to clean up project scaffold, reorganize omniproduct

--- src/main/java/com/example/model/Department.java
+++
@@ -1,3 +0,0 @@
-package com.example.model;
-
-public record Department(String name, String budget) {}
--- src/main/java/com/example/model/Employee.java
+++
@@ -1,3 +0,0 @@
-package com.example.model;
-
-public record Employee(String id, String name, Department department) {}
